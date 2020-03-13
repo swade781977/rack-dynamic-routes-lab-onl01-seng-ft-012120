1df66b53233ad841bd399edfb0f02900814ede76
Chris Wade <swade781977@yahoo.com>
2020-03-13 08:29:15 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,8 +5,8 @@ def call(env)
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@items.include?(item_name) 
-        item = @@items.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
+      if item != nil 
         resp.write "#{item.price}"
         resp.status = 200
       else
